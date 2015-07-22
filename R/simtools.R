efa0080584c90fd2e9a337ec9afd9e2be6bbc238
Francis DiTraglia <fditra@sas.upenn.edu>
2015-07-22 12:57:39 -0400

    finish prelim functions for constructing TeX tables of sim results

--- R/simtools.R
+++ R/simtools.R
@@ -1,38 +1,43 @@
-params <- expand.grid(p = 1:6/10, r = 0:5/10, B = 1:3, N = c(50, 100, 500))
-results <- data.frame(params, size = rpois(nrow(params), 75))
+params <- expand.grid(p = 1:6/100, r = 0:5/10, B = 1:3, N = c(10, 20, 30))
+results <- data.frame(params, size = rowSums(params))
 
-rtables <- xtabs(size ~ r + p + N + B, results)
-#rtables <- xtabs(size ~ r + p + N, results)
-dim_vals <- dimnames(rtables)
-dim_names <- names(dim_vals)
-dim_labels <- lapply(seq_along(dim_vals),
+rtables <- function(table_formula, results_frame){
+  # xtabs sums over all unique combinations of factors
+  # here there is only one value per combination
+  tables <- xtabs(table_formula, results_frame)
+  dim_vals <- dimnames(tables)
+  dim_names <- names(dim_vals)
+  dim_labels <- lapply(seq_along(dim_vals),
                      function(i) paste(dim_names[i], "=", dim_vals[[i]]))
-
-list_names <- apply(expand.grid(dim_labels[3:length(dim(rtables))]),
+  list_names <- apply(expand.grid(dim_labels[3:length(dim(tables))]),
                     1, paste, collapse = ', ')
-table_list <- plyr::alply(rtables, 3:length(dim(rtables)))
-names(table_list) <- list_names
+  table_list <- plyr::alply(tables, 3:length(dim(tables)))
+  names(table_list) <- list_names
+  return(table_list)
+}
 
-# Then lapply a slightly modified version of TeXtable
-# Optionally escape greek letters
-
-
-
-TeXtable <- function(Rtable){
-  nRow <- nrow(Rtable)
-  nCol <- ncol(Rtable)
-  body <- rbind(colnames(Rtable), apply(Rtable, c(1,2), format))
+TeXtable <- function(xtab, tab_name = "", row_lab = NULL, col_lab = NULL){
+  nRow <- nrow(xtab)
+  nCol <- ncol(xtab)
+  if(is.null(row_lab)) row_lab <- names(dimnames(xtab))[1]
+  if(is.null(col_lab)) col_lab <- names(dimnames(xtab))[2]
+  body <- rbind(colnames(xtab), format(xtab))
+  body <- cbind(c(tab_name, rownames(xtab)), body)
   body <- apply(body, c(1,2), function(x) paste0('$', x, '$'))
   rownames(body) <- NULL
   colnames(body) <- NULL
+  if(tab_name == "") body[1,1] <- ""
   body <- apply(body, 1, function(x) paste(x, collapse = ' & '))
   header <- body[1]
   body <- body[-1]
-  body[nRow %/% 2] <- paste0("$\\gamma\\quad$", body[nRow %/% 2])#rowLegend
+  row_lab_i <- nRow %/% 2
+  body[row_lab_i] <- paste0("$", row_lab, "\\;\\;\\;$ ", body[row_lab_i])
   body <- paste(body, collapse = "\\\\ \n")
-  colLegend <- paste0("&\\multicolumn{", nCol - 1, "}{c}{$\\rho$}")
-  header <- paste("\\hline\\hline\n", colLegend, "\\\\ \n", header, "\\\\ \n \\hline")
-  header <- paste0("\\begin{tabular}{r|", paste(rep("r", nCol - 1), collapse = ""), "}\n", header)
+  colLegend <- paste0("&\\multicolumn{", nCol, "}{c}{$", col_lab, "$}")
+  header <- paste("\\hline\\hline\n", colLegend, "\\\\ \n",
+                  header, "\\\\ \n \\hline")
+  header <- paste0("\\begin{tabular}{r|",
+                   paste(rep("r", nCol), collapse = ""), "}\n", header)
   footer <- "\\\\ \n \\hline \n \\end{tabular}"
   return(paste0(header, body, footer))
 }
