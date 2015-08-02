4cb3414f9bad41f58324408fdc77981b57b1ced7
Francis DiTraglia <fditra@sas.upenn.edu>
2015-08-02 17:25:37 -0400

    finish generic functions for FMSC limit experiment: checked against OLS vs IV prelim version and everything seems to be working correctly.

--- R/fmsc_limit_sim.R
+++ R/fmsc_limit_sim.R
@@ -26,30 +26,33 @@ pfmsc <- function(x, tau, bias_coef, tau_sd, efficient_sd){
   return(out)
 }
 
-# qfmsc <- function(p, tau, pi_sq){
-#   sqrt2 <- 1.414214
-#   tau_sd <- sqrt((1 - pi_sq) / pi_sq)
-#   u_min <- 1.1 * tau_sd * sqrt2
-#   #Find x1 such that pfmscOLSvsIV(x1, tau, pi_sq) - p > 0
-#   a1 <- qnorm(1 - (1 - p)/4, 0, tau_sd)
-#   u1 <- max(abs(tau - a1), abs(tau + a1), u_min)
-#   x1 <- qnorm(1 - (1 - p)/4, tau + u1, 1)
-#   #Find x2 such that pfmscOLSvsIV(x2, tau, pi_sq) - p < 0
-#   a2 <- qnorm(1 - p/4, 0, tau_sd)
-#   u2 <- max(abs(tau - a2), abs(tau + a2), u_min)
-#   x2 <- qnorm(p/4, tau - u2, 1)
-#   x_lower <- min(x1, x2)
-#   x_upper <- max(x1, x2)
-#   #Potentially tighter bounds depending on parameters
-#   c <- tau / tau_sd
-#   Kplus <- pnorm(sqrt2 - c)
-#   Kminus <- pnorm(-sqrt2 - c)
-#   z1 <- tau + qnorm(fmscr::clip(p / (Kplus - Kminus), 0, 1))
-#   z2 <- tau + qnorm(fmscr::clip(1 + (p - 1) / Kplus, 0, 1))
-#   z_lower <- min(z1, z2)
-#   z_upper <- max(z1, z2)
-#   lower <- max(x_lower, z_lower)
-#   upper <- min(x_upper, z_upper)
-#   uniroot(function(x) pfmscOLSvsIV(x, tau, pi_sq) - p,
-#           interval = c(lower, upper))$root
-# }
+qfmsc <- function(p, tau, bias_coef, tau_sd, efficient_sd){
+  u_min <- 1.1 * tau_sd * sqrt(2)
+  #Find x1 such that pfmsc(x1, ...) - p > 0
+  a1 <- qnorm(1 - (1 - p)/4, 0, tau_sd)
+  u1 <- max(abs(tau - a1), abs(tau + a1), u_min)
+  m1 <- bias_coef * tau - min(-bias_coef * u1, bias_coef * u1)
+  x1 <- qnorm(1 - (1 - p)/4, m1, efficient_sd)
+  #Find x2 such that pfmsc(x2, ...) - p < 0
+  a2 <- qnorm(1 - p/4, 0, tau_sd)
+  u2 <- max(abs(tau - a2), abs(tau + a2), u_min)
+  m2 <- bias_coef * tau - max(-bias_coef * u2, bias_coef * u2)
+  x2 <- qnorm(p/4, m2, efficient_sd)
+  x_lower <- min(x1, x2)
+  x_upper <- max(x1, x2)
+  #Additional bounds, one of which is typically much tighter
+  #although the other can be trivial. Substantially speeds up
+  #the root finder.
+  Kplus <- pnorm(sqrt(2) - tau/tau_sd)
+  Kminus <- pnorm(-sqrt(2) - tau/tau_sd)
+  z1 <- bias_coef * tau +
+    efficient_sd * qnorm(fmscr::clip(p / (Kplus - Kminus), 0, 1))
+  z2 <- bias_coef * tau + (bias_coef < 0) * bias_coef * tau_sd * sqrt(2) +
+    efficient_sd * qnorm(fmscr::clip(1 + (p - 1) / Kplus, 0, 1))
+  z_lower <- min(z1, z2)
+  z_upper <- max(z1, z2)
+  lower <- max(x_lower, z_lower)
+  upper <- min(x_upper, z_upper)
+  uniroot(function(x) pfmsc(x, tau, bias_coef, tau_sd, efficient_sd) - p,
+          interval = c(lower, upper))$root
+}
